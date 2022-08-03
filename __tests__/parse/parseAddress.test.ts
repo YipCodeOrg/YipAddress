dac51205049e86520eb9854a78608b09021bd022
Colm Bhandal <bhandalc@gmail.com>
2022-08-03 17:33:46 +0100

    More parse address tests
    Mostly testing for removal of whitespace

--- __tests__/parse/parseAddress.test.ts
+++ __tests__/parse/parseAddress.test.ts
@@ -2,7 +2,16 @@ import { Address } from "../../core/address"
 import { parseStrToAddress } from "../../parse/parseAddress"
 
 describe("parseStrToAddress", () => {
-    describe.each([{rawAddress: "", rawAddressDesc: "empty", expectedLines: [], expectedAliasMap: {}}])
+    describe.each([{rawAddress: "", rawAddressDesc: "empty", expectedLines: [], expectedAliasMap: {}},
+        {rawAddress: "Ending in slash n\n" +
+        "\r\t Starting with slash r with leading and trailing space\t \n" +
+        "\r\r\r\n\r\n\nEnding in slash r slash n\r\n" +
+        "  \t   \t \r\t  \r\n  \t \t\t\t\n", rawAddressDesc: "mixed",
+        expectedLines: ["Ending in slash n",
+            "Starting with slash r with leading and trailing space",
+            "Ending in slash r slash n"],
+        expectedAliasMap: {line1: 0, line2: 1, line3: 2}
+    }])
     ('$rawAddressDesc raw address', ({rawAddress, expectedLines, expectedAliasMap}) => {
 
         let parsedAddress: Address
