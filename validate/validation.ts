d10ee9a635a4419d21349549625603620397f73b
Colm Bhandal <bhandalc@gmail.com>
2022-08-27 17:30:09 +0100

    Validation has errors/warnings

--- validate/validation.ts
+++ validate/validation.ts
@@ -2,3 +2,17 @@ export type ValidationResult = {
     errors: string[],
     warnings: string[]
 }
+
+export function hasErrors(r: ValidationResult | null) {
+    if(r === null){
+        return false
+    }
+    return r.errors.length > 0
+}
+
+export function hasWarnings(r: ValidationResult | null) {
+    if(r === null){
+        return false
+    }
+    return r.warnings.length > 0
+}
