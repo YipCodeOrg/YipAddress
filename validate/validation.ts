f7c58080d51d4a42e00e123dbd014632a9f8d2de
Colm Bhandal <bhandalc@gmail.com>
2022-08-28 11:11:08 +0100

    Copy / new validation

--- validate/validation.ts
+++ validate/validation.ts
@@ -3,9 +3,24 @@ export type ValidationResult = {
     warnings: string[]
 }
 
+export function copyValidationResult({errors, warnings}: ValidationResult)
+: ValidationResult {
+    return {
+        errors: [...errors],
+        warnings: [...warnings]
+    }
+}
+
 export const EmptyValidationResult: ValidationResult = {
     errors: [],
     warnings: []
+}
+
+export function newEmptyValidationResult(){
+    return {
+        errors: [],
+        warnings: []
+    }
 }
 
 export function hasErrors(r: ValidationResult | null) {
