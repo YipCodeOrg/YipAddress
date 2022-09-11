78456b62803bc5c03db47f6322656823b63b9a23
Colm Bhandal <bhandalc@gmail.com>
2022-09-11 23:39:03 +0100

    Generalised key press handler

--- util/event.ts
+++ util/event.ts
@@ -1,13 +1,18 @@
 import {KeyboardEvent} from "react"
 
 export function handleEnterKeyPress<T = Element>(f: () => void){
-    return handleKeyPress<T>(f, "Enter")
+    return handleKeyPress<T>({Enter: f})
 }
 
-function handleKeyPress<T = Element>(f: () => void, key: string){
-    return (e: KeyboardEvent<T>) => {
-        if(e.key === key){
-            f()
+export type KeyPressMap = {
+    [key: string]: () => void
+}
+
+export function handleKeyPress<T = Element>(map: KeyPressMap){
+    return (e: KeyboardEvent<T>) => {        
+        const handler = map[e.key]
+        if(handler !== undefined){
+            handler()
         }
     }
 }
