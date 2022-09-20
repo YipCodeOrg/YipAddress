4de70070422f1eddbd722b6898012a135f6e4652
Colm Bhandal <bhandalc@gmail.com>
2022-09-20 12:24:38 +0100

    Enhanced validation
    Lazy enhanced validation class
    Lazily extracts various properties from a raw validation

---
+++ validate/ehancedValidation.ts
@@ -0,0 +1,58 @@
+import { hasErrors, hasWarnings, printMessages, ValidationResult, ValidationSeverity } from "./validation"
+
+export type EnhancedValidation = {
+    validation: ValidationResult,
+    errorMessages: string,
+    warningMessages: string,
+    hasErrors: boolean,
+    hasWarnings: boolean
+}
+
+export function lazyEnhancedValidation(validation: ValidationResult) : EnhancedValidation{
+    return new LazyEnhancedValidation(validation)
+}
+
+class LazyEnhancedValidation implements EnhancedValidation {
+    #validation: ValidationResult
+    #errorMessages: Lazy<string>
+    #warningMessages: Lazy<string>
+    #hasErrors: Lazy<boolean>
+    #hasWarnings: Lazy<boolean>
+
+    constructor(validation: ValidationResult){
+        this.#validation = validation
+        this.#errorMessages = new Lazy(() => printMessages(validation, ValidationSeverity.ERROR))
+        this.#warningMessages = new Lazy(() => printMessages(validation, ValidationSeverity.WARNING))
+        this.#hasErrors = new Lazy(() => hasErrors(validation))
+        this.#hasWarnings = new Lazy(() => hasWarnings(validation))
+    }
+
+    get validation(): ValidationResult{
+        return this.#validation
+    }
+
+    get errorMessages(): string{
+        return this.#errorMessages.value
+    }
+
+    get warningMessages(): string{
+        return this.#warningMessages.value
+    }
+
+    get hasErrors(): boolean{
+        return this.#hasErrors.value
+    }
+
+    get hasWarnings(): boolean{
+        return this.#hasWarnings.value
+    }
+}
+
+class Lazy<T> {
+    #val: T | undefined
+    #init: () => T    
+    constructor(init: () => T) {this.#init = init}
+    public get value(): T {
+      return this.#val ??= this.#init();
+    }
+}
