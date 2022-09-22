656d110c84d57c1aa8474f3183b3d69013697ebd
Colm Bhandal <bhandalc@gmail.com>
2022-09-22 18:38:27 +0100

    Timeout rejected promise

--- util/misc.ts
+++ util/misc.ts
@@ -7,6 +7,10 @@ export function splitNewlines(s: string): string[]{
  * @param t Object to resolve the Promise
  * @param delayMilis Delay by this much before resolving the Promise
  */
- export function timeoutPromiseOf<T>(t: T, delayMilis: number) : Promise<T>{
+export function timeoutPromiseOf<T>(t: T, delayMilis: number) : Promise<T>{
     return new Promise<T>(resolve => setTimeout(() => resolve(t), delayMilis))
+}
+
+export function timeoutRejectedPromiseOf<T>(delayMilis: number) : Promise<T>{
+    return new Promise<T>((_, reject) => setTimeout(() => reject(), delayMilis))
 }
