42d2af8952bd6f686b24f08188f3dd9cc62eec4e
Colm Bhandal <bhandalc@gmail.com>
2022-09-29 12:39:35 +0100

    isBoolean tests

--- __tests__/util/typePredicates.test.ts
+++ __tests__/util/typePredicates.test.ts
@@ -1,4 +1,4 @@
-import { isString } from "../../util/typePredicates"
+import { isBoolean, isString } from "../../util/typePredicates"
 
 describe("isString", () => {
     describe("false", () => {
@@ -55,6 +55,69 @@ describe("isString", () => {
                 it("returns true", () => {                    
                     //Act
                     const actual = isString(val)
+
+                    //Assert
+                    expect(actual).toBe(true)
+                })
+        })
+    })
+})
+
+describe("isBoolean", () => {
+    describe("false", () => {
+        describe.each([
+            {
+                val: null,
+                desc: "Null object"
+            },
+            {
+                val: undefined,
+                desc: "Undefined object"
+            },
+            {
+                val: 234,
+                desc: "Number"
+            },
+            {
+                val: new Date(),
+                desc: "Date"
+            },
+            {
+                val: {foo: "Hello", bar: "World"},
+                desc: "Simple object"
+            },
+        ])(
+            "$desc", ({val}) => {
+
+                it("returns false", () => {                    
+                    //Act
+                    const actual = isBoolean(val)
+
+                    //Assert
+                    expect(actual).toBe(false)
+                })
+        })
+    })
+    describe("true", () => {
+        describe.each([
+            {
+                val: true,
+                desc: "True"
+            },
+            {
+                val: false,
+                desc: "False"
+            },
+            {
+                val: 1 === 1 || "HelloWorld",
+                desc: "Exrpression including truthy but non-boolean values"
+            },
+        ])(
+            "$desc", ({val}) => {
+
+                it("returns true", () => {                    
+                    //Act
+                    const actual = isBoolean(val)
 
                     //Assert
                     expect(actual).toBe(true)
