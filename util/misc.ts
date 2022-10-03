a48177d7d8a70f2095d1c5fc8d6faa5e0e21249e
Colm Bhandal <bhandalc@gmail.com>
2022-10-03 15:26:09 +0100

    More composition HOFs

--- util/misc.ts
+++ util/misc.ts
@@ -25,9 +25,22 @@ export function compose2PairDomain<A1, A2, B, C>(f: (a1: A1, a2: A2) => B, g: (b
     return (a1: A1, a2: A2) => g(f(a1, a2))
 }
 
+export function compose2TripleDomain<A1, A2, A3, B, C>(f: (a1: A1, a2: A2, a3: A3) => B, g: (b: B) => C){
+    return (a1: A1, a2: A2, a3: A3) => g(f(a1, a2, a3))
+}
+
 export function compose2Higher<A, B, X, Y, Z>(f: (a: A) => (x: X) => Y, g: (b: B) => (y: Y) => Z)
     : (a: A, b: B) => (x: X) => Z{
     return (a: A, b: B) => compose2(f(a), g(b))
+}
+
+export function compose3Higher<A1, A2, A3, X1, X2, X3, X4>(
+    f1: (_: A1) => (_: X1) => X2,
+    f2: (_: A2) => (_: X2) => X3,
+    f3: (_: A3) => (_: X3) => X4
+    )
+    : (a1: A1, a2: A2, a3: A3) => (_: X1) => X4{
+    return (a1: A1, a2: A2, a3: A3) => compose2(compose2(f1(a1), f2(a2)), f3(a3))
 }
 
 /** Lifts a potentially undefined function into a defined function, yielding a no-op for undefined. */
